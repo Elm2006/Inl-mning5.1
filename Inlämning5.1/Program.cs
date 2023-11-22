c31f95417b8982d9f160daa0c930c1e5076f60a8
Elm2006 <isel06002@utb.vaxjo.se>
2023-11-22 10:07:36 +0100

    Add project files.

---
+++ Inlämning5.1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace inlämning5.__1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] mening = new string[] { "Dett är en mening", "Detta är en till mening", "Wow tre i rad" };
+            for (int i = 0; i < mening.Length; i++)
+            {
+                Console.WriteLine(mening[i]);
+                Console.WriteLine();
+            }
+        }
+    }
+}
